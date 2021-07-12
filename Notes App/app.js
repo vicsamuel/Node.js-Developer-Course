5405b3055734ecf1423a80765c06be4b7826d2bf
Vic <61720168+vicsamuel@users.noreply.github.com>
2021-07-12 13:49:02 -0400

    Udemy Course

---
+++ Notes App/app.js
@@ -0,0 +1,10 @@
+const validator = require('validator')
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
+console.log(validator.isEmail('andrew@example.com')) 
+
+console.log(validator.isURL('www.google.com')) 
