a78cd984c79a61a101a02c6b8a26e5a1d5c9f89e
Vic <vsamuel1121@gmail.com>
2021-07-12 16:07:26 -0400

    More changes

--- Notes App/app.js
+++ Notes App/app.js
@@ -1,68 +1,63 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: "Note title",
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: "Note body",
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: function(argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
-    }
+	command: 'add',
+	describe: 'Add a new note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'Note body',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.addNote(argv.title, argv.body)
+	}
 })
 
 // Create remove command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
-    }
+	command: 'remove',
+	describe: 'Remove a note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.removeNote(argv.title)
+	}
 })
 
 // Create list command
 yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler: function () {
-        console.log('Listing the notes')
-    }
+	command: 'list',
+	describe: 'List your notes',
+	handler: function () {
+		console.log('Listing out all notes')
+	}
 })
 
-// Create remove command
+// Create read command
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: function () {
-        console.log('Reading note')
-    }
+	command: 'read',
+	describe: 'Read a note',
+	handler: function () {
+		console.log('Reading a note')
+	}
 })
 
 yargs.parse()
-
-// console.log(yargs.argv)
-
-
-// const command = process.argv[2]
-
-// if (command === 'add') {
-//     console.log('Adding note!')
-// } else if (command === 'remove') {
-//     console.log('Removing note!')
-// }
